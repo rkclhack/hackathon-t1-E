9c17db6afc41e42e272914b515816747d8b9c475
YuyMat <yuya2005m@gmail.com>
2025-07-31 14:47:36 +0900

    change tab count

--- chatapp/src/components/ImportantChat.vue
+++ chatapp/src/components/ImportantChat.vue
@@ -5,15 +5,15 @@ import toJpnTime from "../utils/toJpnTime"
 const chatList = inject("chatList")
 
 const importantChatList = computed(() =>
-    chatList.filter((chat) => chat.isImportant === true)
+  chatList.filter((chat) => chat.isImportant === true)
 )
 </script>
 
 <template>
-    <ul>
-        <li v-for="(importantChat, i) in importantChatList" :key="i">
-            <div class="item mt-4">{{ toJpnTime(importantChat.sendAt) }}</div>
-            <div style="white-space: pre-wrap;" class="item mt-4">{{ importantChat.chatContent }}</div>
-        </li>
-    </ul>
+  <ul>
+    <li v-for="(importantChat, i) in importantChatList" :key="i">
+      <div class="item mt-4">{{ toJpnTime(importantChat.sendAt) }}</div>
+      <div style="white-space: pre-wrap;" class="item mt-4">{{ importantChat.chatContent }}</div>
+    </li>
+  </ul>
 </template>
